63b4b3c13f71f8f9c72540efca498bde44879de9
FTwOoO <750207+FTwOoO@users.noreply.github.com>
2020-12-09 10:05:26 +0800

    add ahas sentinel

--- thirdparty/sentinel/ahas.go
+++ thirdparty/sentinel/ahas.go
@@ -1,7 +1,9 @@
 package sentinel
 
 import (
+	sentinel "github.com/alibaba/sentinel-golang/api"
 	config2 "github.com/alibaba/sentinel-golang/core/config"
+	"github.com/alibaba/sentinel-golang/logging"
 	ahas "github.com/aliyun/aliyun-ahas-go-sdk"
 	"github.com/aliyun/aliyun-ahas-go-sdk/config"
 	"os"
@@ -12,4 +14,12 @@ func AHASInit(license string, appName string) error {
 	_ = os.Setenv(config.EnvironmentEnvKey, config.DeployEnvProd)
 	_ = os.Setenv(config2.AppNameEnvKey, appName)
 	return ahas.InitAhasDefault()
+}
+
+func SentinelInit() error {
+	conf := config2.NewDefaultConfig()
+	// for testing, logging output to console
+	conf.Sentinel.Log.Logger = logging.NewConsoleLogger()
+	err := sentinel.InitWithConfig(conf)
+	return err
 }
